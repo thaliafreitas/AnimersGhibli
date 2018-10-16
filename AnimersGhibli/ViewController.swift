15f0c13d9b690c31200741d381963e4e5ae0e123
Thalia Freitas <thaliafreitas@10-45-48-196.0200.ifce.br.iosda.org>
2018-10-16 09:57:27 -0300

    First commit

---
+++ AnimersGhibli/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimersGhibli
+//
+//  Created by Thalia Freitas on 16/10/18.
+//  Copyright © 2018 Thalia Freitas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
