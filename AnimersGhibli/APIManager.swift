86932200383cb292f62622039b62d1de6c4f7182
Thalia Freitas <thaliafreitas@10-45-48-196.0200.ifce.br.iosda.org>
2018-10-16 11:10:23 -0300

    Adding storyboard

---
+++ AnimersGhibli/APIManager.swift
@@ -0,0 +1,13 @@
+//
+//  APIManager.swift
+//  AnimersGhibli
+//
+//  Created by Thalia Freitas on 16/10/18.
+//  Copyright © 2018 Thalia Freitas. All rights reserved.
+//
+
+import UIKit
+
+class APIManager: NSObject {
+
+}
