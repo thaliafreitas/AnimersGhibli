e592742c367f1a1f71a0357e2be3f29d9434ebd9
Thalia Freitas <thaliafreitas@10-45-48-196.0200.ifce.br.iosda.org>
2018-10-23 11:47:59 -0300

    Ädding quiz view controller

--- AnimersGhibli/Controllers/QuizViewController.swift
+++ AnimersGhibli/Controllers/QuizViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+    @IBOutlet weak var questionLbl: UILabel!
+    @IBOutlet weak var answer1: UIButton!
+    @IBOutlet weak var answer2: UIButton!
+    @IBOutlet weak var answer3: UIButton!
+    @IBOutlet weak var answer4: UIButton!
+    @IBOutlet weak var resultLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
-
+    @IBAction func submitAnswer1(_ sender: Any) {
+    }
+    @IBAction func submitAnswer2(_ sender: Any) {
+    }
+    @IBAction func submitAnswer3(_ sender: Any) {
+    }
+    @IBAction func submitAnswer4(_ sender: Any) {
+    }
+    
 }
---
+++ AnimersGhibli/Controllers/ScoreViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ScoreViewController.swift
+//  AnimersGhibli
+//
+//  Created by Thalia Freitas on 23/10/18.
+//  Copyright © 2018 Thalia Freitas. All rights reserved.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    @IBOutlet weak var scoreLbl: UILabel!
+    @IBOutlet weak var resultsLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+
+}
